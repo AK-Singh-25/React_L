592dd1a03d99f40e896986f5793a077ecad910db
AK-Singh-25 <168993543+AK-Singh-25@users.noreply.github.com>
2026-04-23 23:59:02 +0530

    Counter using Hooks

---
+++ 02counterProject/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+
+  let [counter,setCounter]=useState(0)
+
+  const addValue=()=>{
+    counter===20?counter:setCounter(++counter)
+  }
+  const removeValue=()=>{
+    counter===0?counter:setCounter(--counter)
+  }
+
+  return (
+    <>
+      <h1>Hooks and Project</h1>
+      <h2>Counter Value : {counter}</h2>
+
+      <span><button onClick={addValue}>Add Value</button></span>
+      <br />
+      <span><button onClick={removeValue}>Remove Value</button></span>
+    </>
+  )
+}
+
+export default App
